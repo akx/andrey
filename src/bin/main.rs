cc86a16a43d10c5e5d2fa7a9c0909f9d977c2478
Aarni Koskela <akx@iki.fi>
2020-03-08 22:19:33 +0200

    Still work to do

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,25 +1,48 @@
+#[macro_use]
+extern crate clap;
+
 use andrey::read::generate_map;
 use andrey::sampling::{pick_key, temperature_sample};
-use andrey::types::Options;
+use andrey::types::{CounterMap, Options};
 
-fn main() {
-    let opt: Options = Options { state_size: 3 };
-    let map = generate_map(&opt, "test.txt").expect("worked");
+fn sample_forever(options: &Options, map: &CounterMap, temperature: f32) {
     let mut rng = rand::thread_rng();
     let mut head = pick_key(&mut rng, &map).to_owned();
-    let temperature = 6.3f32;
     loop {
         // if we reach a state where the current head is not in the map, start a new line
         while !map.contains_key(&head) {
+            //            eprintln!("Not finding {:?}", head);
             println!();
-            head = pick_key(&mut rng, &map).to_owned();
+            head = "".to_owned();
+            //head = pick_key(&mut rng, &map).to_owned();
         }
         let tails = map.get(&head).expect("tail found");
         let ch = temperature_sample(&mut rng, tails, temperature).expect("sample ok");
+        //eprintln!("[{:?}] => ch {:?}", head, ch);
         if *ch != '\0' {
             print!("{}", ch);
         }
         head.push(*ch);
-        head = head.chars().skip(1).collect();
+        if head.len() > options.state_size {
+            head = head.chars().skip(1).collect();
+        }
     }
+}
+
+fn main() {
+    let matches = clap_app!(myapp =>
+        (version: crate_version!())
+        (author: crate_authors!())
+        (@arg state_size: -s +takes_value +required "State size")
+        (@arg temperature: -t +takes_value +required "Sampling temperature")
+        (@arg input: -i +takes_value +required "Input file")
+    )
+    .get_matches();
+    let opt: Options = Options {
+        state_size: value_t!(matches, "state_size", usize).expect("valid integer"),
+    };
+    let map = generate_map(&opt, matches.value_of("input").expect("got input")).expect("worked");
+    let temperature = value_t!(matches, "temperature", f32).expect("valid float");
+
+    sample_forever(&opt, &map, temperature);
 }
--- src/read.rs
+++ src/read.rs
@@ -4,25 +4,34 @@ use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader};
 
+fn add(map: &mut CounterMap, head: String, next: char) {
+    //    eprintln!("[{:?}] => {:?}", head, next);
+    map.entry(head)
+        .or_insert_with(HashMap::new)
+        .entry(next)
+        .and_modify(|e| *e += 1)
+        .or_insert(1);
+}
+
+fn add_from_trail(map: &mut CounterMap, trail: &VecDeque<char>, next: char) {
+    let head: String = trail.clone().into_iter().collect();
+    add(map, head.trim_start_matches('\0').to_owned(), next);
+}
+
 pub fn generate_map(options: &Options, filename: &str) -> io::Result<CounterMap> {
     let mut map: CounterMap = CounterMap::new();
     let f = File::open(filename)?;
     let f = BufReader::new(f);
     for line in f.lines() {
         let mut trail: VecDeque<char> = VecDeque::with_capacity(options.state_size);
-        for _ in 0..options.state_size {
-            trail.push_back('\0');
-        }
         for ch in line?.chars() {
-            let head: String = trail.clone().into_iter().collect();
-            trail.pop_front();
+            add_from_trail(&mut map, &trail, ch);
+            if trail.len() >= options.state_size {
+                trail.pop_front();
+            }
             trail.push_back(ch);
-            map.entry(head)
-                .or_insert_with(HashMap::new)
-                .entry(ch)
-                .and_modify(|e| *e += 1)
-                .or_insert(1);
         }
+        //        add_from_trail(&mut map, &trail, '\0');
     }
     Ok(map)
 }
